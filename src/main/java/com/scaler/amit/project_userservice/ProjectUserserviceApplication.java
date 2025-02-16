ad6a62644eb3c3d4a2a7654486938fa6f7ff6a48
amitmehta709 <56817772+amitmehta709@users.noreply.github.com>
2025-02-17 00:04:01 +0530

    initial commit

---
+++ src/main/java/com/scaler/amit/project_userservice/ProjectUserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.amit.project_userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUserserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectUserserviceApplication.class, args);
+    }
+
+}
